5d29620024aed7c4f3f5eb303b001067079969c2
markkwintkiewicz <154352369+markkwintkiewicz@users.noreply.github.com>
2024-02-20 16:18:26 +0100

    Add files via upload
    this was another file that was used to practice using the import turtle()

---
+++ main.py
@@ -0,0 +1,19 @@
+# from turtle import Turtle, Screen
+# from another_module import another_variable
+# franklin = Turtle()
+#
+# print(franklin)
+# franklin.shape("turtle")
+# franklin.color("brown")
+# franklin.forward(100)
+# franklin.left(90)
+# franklin.forward(50)
+# franklin.home()
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# C:\Users\Mark\PycharmProjects\turtle_drawing\pythonProject day 16\.venv\Scripts\python.exe C:/Program Files/JetBrains/PyCharm Community Edition 2023.3.1/plugins/python-ce/helpers/pip-20.3.4-py2.py3-none-any.whl/pip install --no-index C:\Program Files\JetBrains\PyCharm Community Edition 2023.3.1\plugins\python-ce\helpers\pip-20.3.4-py2.py3-none-any.whl
+<pip install pretty
